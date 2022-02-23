46d950343be4450b91b57f2485db5f75312ea56b
Master <sharkpass@mail.ru>
2022-02-23 11:43:17 +0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header =() =>{
+    return <header className="header">
+        <img src='https://static.rfstat.com/renderforest/images/v2/logo-homepage/gradient_3.png'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+    return <div className='content'>
+    <div>
+      <img src='https://s-sd.ru/images/cms/thumbs/a5b0aeaa3fa7d6e58d75710c18673bd7ec6d5f6d/animfon_1920_1031_jpg_0_100.jpg'></img>
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        New posts
+      </div>
+      <div>
+        post 1
+      </div>
+      <div>
+        post 2
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
